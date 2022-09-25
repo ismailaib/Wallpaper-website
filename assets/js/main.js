e632b07d8a185cbd7c38896226f29657348bc644
ismailaib <ismailaitbouhmad@gmail.com>
2022-09-25 21:12:05 +0100

    add navbar

---
+++ assets/js/main.js
@@ -0,0 +1,106 @@
+(function() {
+  
+    var Menu = (function() {
+      var burger = document.querySelector('.burger');
+      var menu = document.querySelector('.menu');
+      var menuList = document.querySelector('.menu__list');
+      var brand = document.querySelector('.menu__brand');
+      var menuItems = document.querySelectorAll('.menu__item');
+      
+      var active = false;
+      
+      var toggleMenu = function() {
+        if (!active) {
+          menu.classList.add('menu--active');
+          menuList.classList.add('menu__list--active');
+          brand.classList.add('menu__brand--active');
+          burger.classList.add('burger--close');
+          for (var i = 0, ii = menuItems.length; i < ii; i++) {
+            menuItems[i].classList.add('menu__item--active');
+          }
+          
+          active = true;
+        } else {
+          menu.classList.remove('menu--active');
+          menuList.classList.remove('menu__list--active');
+          brand.classList.remove('menu__brand--active');
+          burger.classList.remove('burger--close');
+          for (var i = 0, ii = menuItems.length; i < ii; i++) {
+            menuItems[i].classList.remove('menu__item--active');
+          }
+          
+          active = false;
+        }
+      };
+      
+      var bindActions = function() {
+        burger.addEventListener('click', toggleMenu, false);
+      };
+      
+      var init = function() {
+        bindActions();
+      };
+      
+      return {
+        init: init
+      };
+      
+    }());
+    
+    Menu.init();
+    
+  }());
+
+
+  const rocketSelector = document.querySelector("[data-rocket]");
+const rocketImageSelector = document.querySelector("[data-rocket-image]");
+
+rocketSelector.addEventListener("mousemove", function (event) {
+  const x = event.clientX - window.innerWidth / 2;
+  const y = event.clientY - window.innerHeight / 2;
+
+  rocketImageSelector.style.transform = `translate3d(${x / 10}px, ${y / 10}px, 0)`;
+});
+
+rocketSelector.addEventListener("mouseleave", function () {
+  rocketImageSelector.style.transform = "translate3d(0, 0, 0)";
+});
+/*==================== MENU SHOW Y HIDDEN ====================*/
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+
+/*==================== ACCORDION SKILLS ====================*/
+
+
+/*==================== QUALIFICATION TABS ====================*/
+
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
