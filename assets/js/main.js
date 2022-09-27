6c91a740ab566acf21e9b72e208af7754cdcbf74
ismailaib <ismailaitbouhmad@gmail.com>
2022-09-27 18:22:54 +0100

    add code

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,31 +52,18 @@
   }());
 
 
-  const rocketSelector = document.querySelector("[data-rocket]");
-const rocketImageSelector = document.querySelector("[data-rocket-image]");
+    function myFunction() {
+      var x = document.getElementById("scrolldown");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
 
-rocketSelector.addEventListener("mousemove", function (event) {
-  const x = event.clientX - window.innerWidth / 2;
-  const y = event.clientY - window.innerHeight / 2;
-
-  rocketImageSelector.style.transform = `translate3d(${x / 10}px, ${y / 10}px, 0)`;
-});
-
-rocketSelector.addEventListener("mouseleave", function () {
-  rocketImageSelector.style.transform = "translate3d(0, 0, 0)";
-});
 /*==================== MENU SHOW Y HIDDEN ====================*/
 
-var myVar;
 
-function myFunction() {
-  myVar = setTimeout(showPage, 0);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "flex";
-}
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
