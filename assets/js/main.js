d087c4c5dbc30537f01a00177dc53149f1535f71
ismailaib <ismailaitbouhmad@gmail.com>
2022-10-18 21:26:55 +0100

    add code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,103 +1,15 @@
-(function() {
-  
-    var Menu = (function() {
-      var burger = document.querySelector('.burger');
-      var menu = document.querySelector('.menu');
-      var menuList = document.querySelector('.menu__list');
-      var brand = document.querySelector('.menu__brand');
-      var menuItems = document.querySelectorAll('.menu__item');
-      
-      var active = false;
-      
-      var toggleMenu = function() {
-        if (!active) {
-          menu.classList.add('menu--active');
-          menuList.classList.add('menu__list--active');
-          brand.classList.add('menu__brand--active');
-          burger.classList.add('burger--close');
-          for (var i = 0, ii = menuItems.length; i < ii; i++) {
-            menuItems[i].classList.add('menu__item--active');
-          }
-          
-          active = true;
-        } else {
-          menu.classList.remove('menu--active');
-          menuList.classList.remove('menu__list--active');
-          brand.classList.remove('menu__brand--active');
-          burger.classList.remove('burger--close');
-          for (var i = 0, ii = menuItems.length; i < ii; i++) {
-            menuItems[i].classList.remove('menu__item--active');
-          }
-          
-          active = false;
-        }
-      };
-      
-      var bindActions = function() {
-        burger.addEventListener('click', toggleMenu, false);
-      };
-      
-      var init = function() {
-        bindActions();
-      };
-      
-      return {
-        init: init
-      };
-      
-    }());
-    
-    Menu.init();
-    
-  }());
+      //common reveal options to create reveal animations
+      ScrollReveal({
+        reset: true,
+        distance: '60px',
+        duration: 2500,
+        delay: 400
+      });
 
-
-    function myFunction() {
-      var x = document.getElementById("scrolldown");
-      if (x.style.display === "none") {
-        x.style.display = "block";
-      } else {
-        x.style.display = "none";
-      }
-    }
-
-/*==================== MENU SHOW Y HIDDEN ====================*/
-
-
-
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-
-
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-
-
-/*==================== REMOVE MENU MOBILE ====================*/
-
-
-/*==================== ACCORDION SKILLS ====================*/
-
-
-/*==================== QUALIFICATION TABS ====================*/
-
-
-/*==================== SERVICES MODAL ====================*/
-
-
-/*==================== PORTFOLIO SWIPER  ====================*/
-
-
-/*==================== TESTIMONIAL ====================*/
-
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-
-
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
-
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+      //target elements, and specify options to create reveal animations
+      ScrollReveal().reveal('.main-title, .section-title', { delay: 300, origin: 'left' });
+      ScrollReveal().reveal('.sec-01 .image, .info', { delay: 400, origin: 'bottom' });
+      ScrollReveal().reveal('.text-box', { delay: 500, origin: 'right' });
+      ScrollReveal().reveal('.media-icons i', { delay: 300, origin: 'bottom', interval: 100 });
+      ScrollReveal().reveal('.sec-02 .image, .sec-03 .image', { delay: 300, origin: 'top' });
+      ScrollReveal().reveal('.media-info li', { delay: 300, origin: 'left', interval: 100 });
