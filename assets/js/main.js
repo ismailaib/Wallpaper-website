8898499de8b2f2efe052cbd140b62f439c1e0f74
ismailaib <ismailaitbouhmad@gmail.com>
2022-10-20 14:52:30 +0100

    add responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,3 +136,18 @@ setInterval(() => {
 }, 3000);
 
 // :)
+
+
+let btnDownload = document.querySelector('button');
+let img = document.querySelector('img');
+
+btnDownload.addEventListener('click' , () => { 
+  let imgPath = img.getAttribute('src'); 
+  let fileName = getFileName(imgPath); 
+
+  saveAs(imgPath, fileName) 
+}); 
+
+function getFileName(str){ 
+  return str.substring(str.lastIndexOf('/')+1); 
+}
